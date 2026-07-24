265dbe91d21c573e6a962133f6ef1e12ccbc9484
vungocnamhust <vungocnam0409@gmail.com>
2026-07-25 00:16:23 +0700

    perf: reduce bundle size by removing backup copies, old intermediate versions, and temporary test files

--- find_overflow.js
+++
@@ -1,6 +0,0 @@
-const docWidth = document.documentElement.clientWidth;
-[].forEach.call(document.querySelectorAll('*'), function(el) {
-  if (el.scrollWidth > docWidth) {
-    console.log(el.tagName + '.' + el.className + ' width: ' + el.scrollWidth);
-  }
-});
